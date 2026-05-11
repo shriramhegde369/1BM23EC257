e0127700a3c5c2747451b10df71746db50f18d3f
shriramhegde369 <shriramtirumaleshwar.ec23@bmsce.ac.in>
2026-05-11 06:55:01 +0530

    Create dummy_dut.sv

---
+++ EDA/task4/dummy_dut.sv
@@ -0,0 +1,11 @@
+//File       : dummy_dut.sv
+//Author     :Shriram T Hegde / 1BM23EC257
+//Created    : 2026-02-5
+//Module     : dummy_dut
+//Project    : SystemVerilog and Verification (23EC6PE2SV)
+//Faculty    : Prof. Ajaykumar Devarapalli
+//Description: A placeholder dummy DUT for the class-based Packet verification lab.
+//------------------------------------------------------------------------------
+
+module dummy_dut;
+endmodule
